9b58022390ae27a9c0c414782dd1db106b743a94
andrea <andrea.zollini@proton.me>
2024-04-18 18:19:13 +0200

    second commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
+import vue from "@astrojs/vue";
+
+import tailwind from "@astrojs/tailwind";
+import { defineModel } from 'vue';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [vue({
+    script: defineModel
+  }), tailwind()]
+});
